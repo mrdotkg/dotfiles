ded7ba0f47fb742593b994be9b760c374556705a
mrdot <grv.rkg@gmail.com>
2025-06-10 09:39:36 +0530

    logo removed

--- gui.ps1
+++ gui.ps1
@@ -69,7 +69,7 @@ else {
 # ------------------------------
 # Main Form
 $FormProps = @{
-    Icon      = [System.Drawing.Icon]::ExtractAssociatedIcon("$PSScriptRoot\gandalf.ico")
+    # Icon      = [System.Drawing.Icon]::ExtractAssociatedIcon("$PSScriptRoot\gandalf.ico")
     Size      = '500,600'
     Text      = "Gray WinUtil"
     BackColor = $script:UI.Colors.Background
